0277b2dd3967cae1734e869f54f17389192dbfa8
Yojan Patel <yojanpatel@gmail.com>
2015-01-18 21:04:11 +0000

    RewardFunction normalized to [-1,1]

--- server/src/main/java/uk/co/yojan/kiara/analysis/learning/rewards/VariedSkipReward.java
+++ server/src/main/java/uk/co/yojan/kiara/analysis/learning/rewards/VariedSkipReward.java
@@ -3,26 +3,26 @@ package uk.co.yojan.kiara.analysis.learning.rewards;
 public class VariedSkipReward implements RewardFunction {
   @Override
   public double rewardSkip(int percent) {
-    return percent < 10 ? -20 :
-           percent < 20 ? -15 :
-           percent < 50 ? -10 :
+    return percent < 10 ? -1 :
+           percent < 20 ? -0.75 :
+           percent < 50 ? -0.5 :
            percent < 75 ? 0 :
-           percent < 90 ? 10 :
+           percent < 90 ? 0.5 :
            rewardTrackFinished();
   }
 
   @Override
   public double rewardQueue() {
-    return 15;
+    return 0.75;
   }
 
   @Override
   public double rewardTrackFinished() {
-    return 15;
+    return 0.75;
   }
 
   @Override
   public double rewardFavourite() {
-    return 20;
+    return 1;
   }
 }
